9904763585704ca1bafb6b8794d815cf8ea0f8d7
Martin Scharm <martin@binfalse.de>
2016-07-12 15:51:59 +0200

    corrected typo in file name

--- example/snippet.py
+++ example/snippet.py
@@ -5,5 +5,5 @@ import requests
 r = requests.post('http://localhost:1234', files={'project': open('example.tex', 'rb')})
 
 # store the results
-with open("/tmp/exmaple.pdf", 'wb') as result:
+with open("/tmp/example.pdf", 'wb') as result:
     result.write(r.content)
